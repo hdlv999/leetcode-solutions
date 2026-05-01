25c90a0a701004b1906b773fe06839e3cc09ee9b
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-01 14:30:52 +0700

    Create max_consecutive_ones.py
    Q3. Max Consecutive Ones

---
+++ quests/data-structures-and-algorithms/max_consecutive_ones.py
@@ -0,0 +1,16 @@
+# Задача:
+# https://leetcode.com/problems/max-consecutive-ones/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_count = 0
+        current_count = 0
+    
+        for num in nums:
+            if num == 1:
+                current_count += 1
+                max_count = max(max_count, current_count)
+            else:
+                current_count = 0
+    
+        return max_count
