0ae11a02c066b0a30d3df175872e226dbf487b9f
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-03 21:05:07 +0700

    Create evaluate_reverse_polish_notation.py
    Q2. Evaluate Reverse Polish Notation

---
+++ quests/data-structures-and-algorithms/evaluate_reverse_polish_notation.py
@@ -0,0 +1,24 @@
+# Задача:
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+    
+        for token in tokens:
+            if token in ['+', '-', '*', '/']:
+                b = stack.pop()
+                a = stack.pop()
+            
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                elif token == '/':
+                    stack.append(int(a / b))
+            else:
+                stack.append(int(token))
+    
+        return stack[0]
