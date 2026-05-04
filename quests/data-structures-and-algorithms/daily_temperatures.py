714dc7aea806d7a9508f06d6d9a8daa2d19a35c6
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-04 20:20:03 +0700

    Create daily_temperatures.py
    Q2. Daily Temperatures

---
+++ quests/data-structures-and-algorithms/daily_temperatures.py
@@ -0,0 +1,17 @@
+# Задача:
+# https://leetcode.com/problems/daily-temperatures/?envType=problem-list-v2&envId=dsa-linear-shoal-monotonic-stack
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+    
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                answer[prev_day] = i - prev_day
+        
+            stack.append(i)
+    
+        return answer
