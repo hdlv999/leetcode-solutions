1c97b213f9cf7ff8c753a9c76551d27c125f1f8c
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-06 22:50:23 +0700

    Create last_stone_weight.py
    Q1. Last Stone Weight

---
+++ quests/data-structures-and-algorithms/last_stone_weight.py
@@ -0,0 +1,18 @@
+# Задача:
+# https://leetcode.com/problems/last-stone-weight/description/?envType=problem-list-v2&envId=dsa-sequence-valley-heap
+
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+    
+        while len(stones) > 1:
+            stone1 = -heapq.heappop(stones)
+            stone2 = -heapq.heappop(stones)
+        
+            if stone1 != stone2:
+                heapq.heappush(stones, -(stone1 - stone2))
+    
+        return -stones[0] if stones else 0
