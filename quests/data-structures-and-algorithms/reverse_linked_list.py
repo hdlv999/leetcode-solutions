6ab60c3d3341f5fb9f0de46c9b8787ee85326b08
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-09 22:40:05 +0700

    Create reverse_linked_list.py
    Q3. Reverse Linked List

---
+++ quests/data-structures-and-algorithms/reverse_linked_list.py
@@ -0,0 +1,20 @@
+# Задача:
+# https://leetcode.com/problems/reverse-linked-list/description/?envType=problem-list-v2&envId=dsa-association-slope-linked-list
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+    
+        while current:
+            next_temp = current.next
+            current.next = prev
+            prev = current
+            current = next_temp
+    
+        return prev
