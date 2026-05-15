3bdd91df2eec4216c9b246ead4fb35039cffd225
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-15 14:30:13 +0700

    Create first_missing_positive.py
    Q3. First Missing Positive

---
+++ quests/data-structures-and-algorithms/first_missing_positive.py
@@ -0,0 +1,17 @@
+# Задача:
+# https://leetcode.com/problems/first-missing-positive/description/?envType=problem-list-v2&envId=dsa-association-slope-hash
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                correct_idx = nums[i] - 1
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        return n + 1
