292764fd992e2ca1d2eed346cc5d32a2b7b859ea
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-15 14:15:08 +0700

    Create copy_list_with_random_pointer.py
    Q2. Copy List with Random Pointer

---
+++ quests/data-structures-and-algorithms/copy_list_with_random_pointer.py
@@ -0,0 +1,39 @@
+# Задача:
+# https://leetcode.com/problems/copy-list-with-random-pointer/description/?envType=problem-list-v2&envId=dsa-association-slope-hash
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        current = head
+        while current:
+            new_node = Node(current.val)
+            new_node.next = current.next
+            current.next = new_node
+            current = new_node.next
+
+        current = head
+        while current:
+            if current.random:
+                current.next.random = current.random.next
+            current = current.next.next
+
+        original = head
+        copy_head = head.next
+        copy = copy_head
+
+        while original:
+            original.next = original.next.next
+            if copy.next:
+                copy.next = copy.next.next
+            original = original.next
+            copy = copy.next
+        
+        return copy_head
