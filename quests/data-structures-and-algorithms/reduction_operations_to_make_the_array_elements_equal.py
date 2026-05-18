af843e8c1ca380d35fc7f5d84be815657cb24014
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-18 11:55:13 +0700

    Create reduction_operations_to_make_the_array_elements_equal.py
    Q2. Reduction Operations to Make the Array Elements Equal

---
+++ quests/data-structures-and-algorithms/reduction_operations_to_make_the_array_elements_equal.py
@@ -0,0 +1,14 @@
+# Задача:
+# https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/description/?envType=problem-list-v2&envId=dsa-sorting-plateau-sorting
+
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+
+        operations = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                operations += i
+
+        return operations
