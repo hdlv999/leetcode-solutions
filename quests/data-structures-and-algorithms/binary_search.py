4cd1d43b6d7e83e55601479b81cac91250ab14fd
volition <280869582+hdlv999@users.noreply.github.com>
2026-05-18 14:30:07 +0700

    Create binary_search.py
    Q2. Binary Search

---
+++ quests/data-structures-and-algorithms/binary_search.py
@@ -0,0 +1,19 @@
+# Задача:
+# https://leetcode.com/problems/binary-search/description/?envType=problem-list-v2&envId=dsa-sorting-plateau-binary-search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
